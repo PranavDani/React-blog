912b94f47a04c67cf26585c97be209871a6f47b6
Pranav Dani <pranavpdani@gmail.com>
2021-08-13 20:36:12 +0530

    Add BlogList

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,31 @@
-const Home = () => {
-  const handleClick = (e) => {
-    console.log("Hello bruh", e);
-  };
+import { useState } from "react";
+import BlogList from "./BlogList";
 
-  const handleClickAgain = (name, e) => {
-    console.log("hello " + name, e.target);
+const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    {
+      title: "Welcome to the party",
+      body: "lorem ipsum...",
+      author: "Doug",
+      id: 2,
+    },
+    {
+      title: "No tips and tricks",
+      body: "lorem ipsum...",
+      author: "Hushari",
+      id: 3,
+    },
+  ]);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
   };
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <button onClick={handleClick}>Click me</button>
-      <button onClick={(e) => handleClickAgain("Bruh", e)}>
-        Click me Again
-      </button>
+      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
     </div>
   );
 };
