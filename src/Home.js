a0d7984b35cf2bf782379634f6b5cdbdc9f6019e
Pranav Dani <pranavpdani@gmail.com>
2021-08-19 17:14:14 +0530

    Add: New Blogs page; establish connection with JSON server

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,18 @@
-import { useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    {
-      title: "Welcome to the party",
-      body: "lorem ipsum...",
-      author: "Doug",
-      id: 2,
-    },
-    {
-      title: "No tips and tricks",
-      body: "lorem ipsum...",
-      author: "Hushari",
-      id: 3,
-    },
-  ]);
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
 };
---
+++ src/useFetch.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  // const handleDelete = (id) => {
+  //   const newBlogs = blogs.filter((blog) => blog.id !== id);
+  //   setBlogs(newBlogs);
+  // };
+
+  useEffect(() => {
+    const abortCont = new AbortController();
+
+    // console.log("use effect ran");
+    fetch(url, { signal: abortCont.signal })
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Could Not fetch the data for that resource");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        // console.log(data);
+        setData(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        if (err.name === "AbortError") {
+          console.log("Fetch aborted bruh");
+        } else {
+          setIsPending(false);
+          setError(err.message);
+        }
+      });
+
+    return () => abortCont.abort();
+  }, [url]);
+
+  return { data, isPending, error };
+};
+
+export default useFetch;
